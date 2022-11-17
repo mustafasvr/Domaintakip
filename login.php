4fefebd34dd31f0080faf193af7d63a4e4ce2aa3
MustafaseveR <mustafasever492@gmail.com>
2022-11-17 19:42:27 +0300

    V:1.0.0

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <title>Kullanıcı giriş sayfası</title>
+</head>
+
+<body>
+
+
+    <div class="container">
+
+        <div class="card col-md-3 position-absolute top-50 start-50 translate-middle">
+            <div class="card-header text-center">
+                <h1>Domain Takip</h1>
+            </div>
+            <div class="card-body">
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+            </div>
+            <div class="card-footer">
+            <button class="btn btn-primary">Yeni hesap oluştur</button>
+                <button class="float-end btn btn-success">Giriş Yap</button>
+            </div>
+        </div>
+
+
+
+
+    </div>
+
+
+</body>
+
+</html>
